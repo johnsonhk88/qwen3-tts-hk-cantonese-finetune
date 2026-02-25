e51b197266eaed8262821e748d790248448b5e61
Johnson Chong <johnsonhk88@gmail.com>
2026-02-25 14:53:19 +0800

    feat: add download script for Cantonese dataset

---
+++ Dataset-Cantonese/download-dataset.sh
@@ -0,0 +1,6 @@
+# use git clone LFS to download the dataset, make sure you have git-lfs installed and configured properly
+# Make sure git-xet is installed (https://hf.co/docs/hub/git-xet)
+curl -sSfL https://hf.co/git-xet/install.sh | sh
+
+# JackyHoCL/cleaned_mixed_cantonese_and_english_speech 
+git clone https://huggingface.co/datasets/JackyHoCL/cleaned_mixed_cantonese_and_english_speech
